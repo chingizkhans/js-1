c612a13744ab57ecfabbcd410ac1ec85aeb4e192
chingizkhans <fltes.shx@gmail.com>
2023-11-22 12:18:49 +0300

    first commit

---
+++ core.js
@@ -0,0 +1,15 @@
+let myName = ""
+myName = "Asan"
+myName = "Uson"
+myName = "Michael"
+
+const myAge = 100
+
+let myHeight = 199
+let myIQ = 299
+let mycolorOfMyHair = "yellow"
+myName = "Doporosbek"
+let myHouseColor = "green"
+let myBrainHeight = "1000"
+let CarIsBlue = "false"
+console.log(myHouseColor)
